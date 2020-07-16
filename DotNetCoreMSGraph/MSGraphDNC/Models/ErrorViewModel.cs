21718d397a4b536804391ab53961991f632d52b2
Fabian Williams <fabianwilliams@Fabians-MacBook-Pro.local>
2020-07-16 19:37:28 -0400

    WIP - errors on Calendar View

---
+++ DotNetCoreMSGraph/MSGraphDNC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSGraphDNC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
