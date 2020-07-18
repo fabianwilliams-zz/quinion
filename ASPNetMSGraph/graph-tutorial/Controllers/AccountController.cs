0dc513ee12689cfd15e576ff7750da99b25137a9
Fabian Williams <fabian@adotob.com>
2020-07-18 13:02:49 -0400

    Demo WOrking  for ASPNET Graph

---
+++ ASPNetMSGraph/graph-tutorial/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using graph_tutorial.TokenStorage;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace graph_tutorial.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // Signal OWIN to send an authorization request to Azure
+                Request.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var tokenStore = new SessionTokenStore(null, System.Web.HttpContext.Current, ClaimsPrincipal.Current);
+                tokenStore.Clear();
+                //this belo was the only thing here in origina, but now implemeting token store ahead of this call
+                Request.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
